dd658189e1ed48e13f922de79f32b833474360ed
chinhle-pa <61530823+chinhle-pa@users.noreply.github.com>
2022-02-16 11:21:03 +0700

    android sdk v4 finished
    android sdk v4 finished

--- src/android/ZaloLoginPlugin.java
+++ src/android/ZaloLoginPlugin.java
@@ -44,9 +44,18 @@ import java.util.Set;
 
 import com.zing.zalo.zalosdk.oauth.LoginVia;
 import com.zing.zalo.zalosdk.oauth.OAuthCompleteListener;
+import com.zing.zalo.zalosdk.oauth.model.ErrorResponse;
 import com.zing.zalo.zalosdk.oauth.OauthResponse;
-import com.zing.zalo.zalosdk.oauth.ValidateOAuthCodeCallback;
+import com.zing.zalo.zalosdk.oauth.ValidateCallback;
 import com.zing.zalo.zalosdk.oauth.ZaloSDK;
+import com.zing.zalo.zalosdk.oauth.ZaloOpenAPICallback;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+import java.util.Base64;
+import java.security.SecureRandom;
 
 public class ZaloLoginPlugin extends CordovaPlugin {
     public static String userId = "";
@@ -63,26 +72,46 @@ public class ZaloLoginPlugin extends CordovaPlugin {
     private CallbackContext lastGraphContext = null;
     private String lastGraphRequestMethod = null;
     private String graphPath;
+    private String codeVerifier = genCodeVerifier();
 
     OAuthCompleteListener listener = new OAuthCompleteListener(){
         @Override
-        public void onAuthenError(int errorCode, String message) {
-            // Đăng nhập thất bại..
-            super.onAuthenError(errorCode, message);
-            try {
-                loginContext.error( new JSONObject("{"+ "\"status\": \"error\",authResponse:{"+"\"errorCode\":"+errorCode+"}}"));
-            } catch (JSONException e) {
-                loginContext.error(errorCode);
-                e.printStackTrace();
-            }
-        };
+        public void onAuthenError(ErrorResponse errorResponse) {
+            //Đăng nhập thất bại..
+            super.onAuthenError(errorResponse);
+            loginContext.error(getErrorResponse(errorResponse));
+        }
     
         @Override
         public void onGetOAuthComplete(OauthResponse response) {
             super.onGetOAuthComplete(response);
             String code = response.getOauthCode();
             //Đăng nhập thành công..
-            loginContext.success(getResponse(response));
+            // loginContext.success(getResponse(response));
+            Context context = cordova.getActivity().getApplicationContext();
+            ZaloSDK.Instance.getAccessTokenByOAuthCode( context, code, codeVerifier, new ZaloOpenAPICallback() {
+                @Override
+                public void onResult(JSONObject data) {
+                    int err = data.optInt("error");
+                    if (err == 0) {
+                        //clearOauthCodeInfo(); //clear used oacode
+            
+                        String access_token = data.optString("access_token");
+                        String refresh_token = data.optString("refresh_token");
+                        long expires_in = Long.parseLong(data.optString("expires_in"));
+                        String[] fields = {"id"};
+            
+                        //Store data token in app cache
+                        ZaloSDK.Instance.getProfile( context, access_token, new ZaloOpenAPICallback(){
+                            @Override
+                            public void onResult(JSONObject response) {
+                                loginContext.success(getResponse(response));
+                            }
+                        }, fields);
+                          
+                    }
+                }
+            });
         };
     };
     @Override
@@ -97,6 +126,10 @@ public class ZaloLoginPlugin extends CordovaPlugin {
             executeLogin(args, callbackContext);
             return true;
         }
+        else if (action.equals("logout")) {
+            executeLogout(args, callbackContext);
+            return true;
+        }
         return false;
     }
 
@@ -108,21 +141,31 @@ public class ZaloLoginPlugin extends CordovaPlugin {
         loginContext.sendPluginResult(pr);
         // Set up the activity result callback to this class
         cordova.setActivityResultCallback(this);
-        ZaloSDK.Instance.authenticate(cordova.getActivity(), LoginVia.APP_OR_WEB, listener);
+        String codeChallenge = genCodeChallenge(codeVerifier);
+        ZaloSDK.Instance.authenticateZaloWithAuthenType(cordova.getActivity(), LoginVia.APP_OR_WEB, codeChallenge, listener);
+    }
+    private void executeLogout(JSONArray args, CallbackContext callbackContext) throws JSONException {
+        ZaloSDK.Instance.unauthenticate();
     }
 
     /**
      * Create a Zalo Response object that matches the one for the Javascript SDK
      * @return JSONObject - the response object
      */
-    public JSONObject getResponse(OauthResponse response) {
+    public JSONObject getResponse(JSONObject response) {
         String result;
-        if (response.getuId()!=0) {
+        Context context = this.cordova.getActivity().getApplicationContext();
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, response.optString("id"), duration);
+        toast.show();
+        
+        if (response.optInt("error")==0) {
             result = "{"
                 + "\"status\": \"connected\","
                 + "\"authResponse\": {"
-                + "\"oauthCode\": \"" + response.getOauthCode() + "\","
-                + "\"userID\": \"" + response.getuId() + "\""
+                + "\"errorCode\": \"" + response.optInt("error") + "\","
+                + "\"userID\": \"" + response.opt("id") + "\""
                 + "}"
                 + "}";
         } else {
@@ -136,5 +179,50 @@ public class ZaloLoginPlugin extends CordovaPlugin {
             e.printStackTrace();
         }
         return new JSONObject();
+    }
+
+    public JSONObject getErrorResponse(ErrorResponse response) {
+        String result;
+        if (response.getErrorCode()!=0) {
+            result = "{"
+                + "\"status\": \"error\","
+                + "\"authResponse\": {"
+                + "\"oauthCode\": \"" + response.getErrorCode() + "\","
+                + "}"
+                + "}";
+        } else {
+            System.out.print(response);
+            result = "{"
+                + "\"status\": \"unknown\""
+                + "}";
+        }
+        try {
+            return new JSONObject(result);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return new JSONObject();
+    }
+
+    public String genCodeVerifier() {  
+        SecureRandom sr = new SecureRandom();  
+        byte[] code = new byte[32];  
+        sr.nextBytes(code);  
+        String verifier = Base64.getUrlEncoder().withoutPadding().encodeToString(code);  
+        return verifier;  
+    }  
+
+    public String genCodeChallenge(String codeVerifier) {  
+        String result = null;  
+        try {  
+            byte[] bytes = codeVerifier.getBytes("US-ASCII");  
+            MessageDigest md = MessageDigest.getInstance("SHA-256");  
+            md.update(bytes, 0, bytes.length);  
+            byte[] digest = md.digest();  
+            result = Base64.getUrlEncoder().withoutPadding().encodeToString(digest);  
+        } catch (Exception ex) {  
+            System.out.println(ex.getMessage());
+        }  
+        return result;  
     }
 }
--- www/zalo.js
+++ www/zalo.js
@@ -1,5 +1,8 @@
 var exec = require('cordova/exec')
 
-exports.login = function (permissions, s, f) {
+exports.login = function(permissions, s, f) {
     exec(s, f, 'ZaloLoginPlugin', 'login', permissions)
+}
+exports.logout = function(s, f) {
+    exec(s, f, 'ZaloLoginPlugin', 'logout')
 }
